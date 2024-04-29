0142f6094e95035dcfde0c36fd3b5bb86e4b6d2e
ihabawad18 <ihabawad767@gmail.com>
2024-04-29 10:38:30 +0300

    pushing buzz presses

--- Assets/buzz_collider_script.cs
+++ Assets/buzz_collider_script.cs
@@ -8,7 +8,31 @@ public class buzz_collider_script : MonoBehaviour
     public Button myButton;
     void OnTriggerEnter(Collider other)
     {
+        if (transform.parent != null)
+        {
+            Transform grandParent = transform.parent.parent;
+            if (grandParent != null)
+            {
+                if (grandParent.gameObject.activeInHierarchy)
+                {
+                    myButton.onClick.Invoke();
 
-        myButton.onClick.Invoke(); 
+                    Debug.Log("pressed button.");
+                }
+                else
+                {
+                    Debug.Log("Cant press the button.");
+                }
+            }
+            else
+            {
+                Debug.Log("No grandparent found.");
+            }
+        }
+        else
+        {
+            Debug.Log("No parent found.");
+        }
+
     }
 }

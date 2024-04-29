7eedd062220f7db047fdd371b13633d69a5e608d
ihabawad18 <ihabawad767@gmail.com>
2024-04-29 16:38:13 +0300

    fixing buttons issue

--- Assets/Scripts/AnswerScript.cs
+++ Assets/Scripts/AnswerScript.cs
@@ -9,6 +9,8 @@ public class AnswerScript : MonoBehaviour
     public bool isCorrect = false;
 
     public QuizManager quizManager;
+    public GameObject[] options;
+
 
     public void Answer()
     {
@@ -28,14 +30,32 @@ public class AnswerScript : MonoBehaviour
             StartCoroutine(ExecuteAfterDelayWrong());
         }
 
-                
+
+    }
+
+    private void enableButtons()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<Button>().interactable = true;
+        }
+    }
+
+    private void disableButtons()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<Button>().interactable = false;
+        }
     }
 
     IEnumerator ExecuteAfterDelayCorrect()
     {
-
+        disableButtons();
         // Wait for the specified delay
+        //yield return new WaitForSeconds(1.65f);
         yield return new WaitForSeconds(1.5f);
+        enableButtons();
 
         quizManager.correct();
 
@@ -44,9 +64,11 @@ public class AnswerScript : MonoBehaviour
 
     IEnumerator ExecuteAfterDelayWrong()
     {
+        disableButtons();
         // Wait for the specified delay
+        //yield return new WaitForSeconds(1.65f);
         yield return new WaitForSeconds(1.5f);
-
+        enableButtons();
         quizManager.wrong();
 
         this.GetComponent<Button>().image.color = Color.white;
